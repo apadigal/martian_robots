125d287343ad88747301da24a56b21b6970dc53a
apadigal <anandkumar.padigala@gmail.com>
2020-07-08 18:57:25 +0100

    feat: added Movement object

---
+++ src/main/java/com/apadigal/martianrobots/bean/Movement.java
@@ -0,0 +1,16 @@
+/*
+ * Swash Tech Ltd.
+ *
+ * Movement.java
+ *
+ * © 2018 Swash Tech Ltd. All Rights Reserved
+ */
+// ---- Package ---------------------------------------------------------------
+package com.apadigal.martianrobots.bean;
+// ---- Import Statements -----------------------------------------------------
+
+public enum Movement {
+    L,
+    R,
+    F
+}
