6e686236cf7aa61400b40d4ae13fd1691684d6c3
apadigal <anandkumar.padigala@gmail.com>
2020-07-08 18:56:38 +0100

    feat: added Orientation and it's test case

---
+++ src/main/java/com/apadigal/martianrobots/bean/Orientation.java
@@ -0,0 +1,46 @@
+/*
+ * Swash Tech Ltd.
+ *
+ * Oriantation.java
+ *
+ * © 2018 Swash Tech Ltd. All Rights Reserved
+ */
+// ---- Package ---------------------------------------------------------------
+package com.apadigal.martianrobots.bean;
+// ---- Import Statements -----------------------------------------------------
+
+public enum Orientation {
+    N,
+    E,
+    W,
+    S;
+
+    public Orientation turnLeft(){
+        switch (this){
+            case N:
+                return W;
+            case E:
+                return N;
+            case W:
+                return S;
+            case S:
+                return E;
+        }
+        return this;
+    }
+
+    public Orientation turnRight(){
+        switch (this){
+            case N:
+                return E;
+            case E:
+                return S;
+            case W:
+                return N;
+            case S:
+                return W;
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/com/apadigal/martianrobots/bean/OrientationTest.java
@@ -0,0 +1,42 @@
+package com.apadigal.martianrobots.bean;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static com.apadigal.martianrobots.bean.Orientation.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OrientationTest {
+
+    @ParameterizedTest
+    @MethodSource("getTurnLeftOrientationData")
+    public void successWhenTurnLeft(Orientation orientation, Orientation expected){
+        assertEquals(expected, orientation.turnLeft());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTurnRightOrientationData")
+    public void successWhenTurnRight(Orientation orientation, Orientation expected){
+        assertEquals(expected, orientation.turnRight());
+    }
+
+    public static Stream<Arguments> getTurnLeftOrientationData(){
+        return Stream.of(Arguments.of(N, W),
+                Arguments.of(E, N),
+                Arguments.of(W, S),
+                Arguments.of(S, E)
+                );
+    }
+
+    public static Stream<Arguments> getTurnRightOrientationData(){
+        return Stream.of(Arguments.of(N, E),
+                Arguments.of(E, S),
+                Arguments.of(W, N),
+                Arguments.of(S, W)
+                );
+    }
+
+}
